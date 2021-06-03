5ea32357c6a401d6dc39b00827b5f1af26002dd0
Jacky Xie <z5257079@cse.unsw.edu.au>
2021-06-03 19:48:25 +1000

    wip blog

--- src/pages/blog/blog.tsx
+++ src/pages/blog/blog.tsx
@@ -1,10 +1,36 @@
 import './blog.css';
+import react from 'react';
 
 export function Blogs() {
+    const [scrollHeight, setScrollHeight] = react.useState(document.scrollHeight);
+    const [elementHeight, setElementHeight] = react.useState(0);
+
+
+    //add event listener listening to scrollheight increase and decrease as window gets resized
+    react.useEffect(()=> {
+        document.addEventListener('resize', ()=> {
+            setScrollHeight(document.scrollHeight);
+        })
+    }, [])
+
+    //checks scroll height has changed and re-renders height of filter
+    react.useEffect(()=> {
+        setElementHeight(document.getElementById('blog-box').scrollHeight);
+        console.log(document.getElementById('blog-box').scrollHeight)
+        console.log(scrollHeight)
+    },[scrollHeight])
 
     return (
 
+
         <div id="blog-box" className="body-container">
+
+            {elementHeight > 0 &&             
+            <div id="filter" style={{height: elementHeight}}>
+                this is in work in progress
+            </div>}
+
+
             <div className="blog-container">
 
                 <div className="blog-content-wrapper">
@@ -46,7 +72,7 @@ export function Blogs() {
                 show more
             </button>
         </div>
-
+        
 
 
     )
